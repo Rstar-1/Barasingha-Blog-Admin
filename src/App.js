bcf94780b501e49d9e1da8248648757f4a295b34
Rstar-1 <rajshetye.5855@gmail.com>
2023-11-04 19:55:37 +0530

    nnn

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import Error from "./pages/error/Error";
 import Sidebar from "./pages/admin/sidebar/Sidebar";
 import Header from "./pages/admin/layouts/header/Header";
 import Dashboard from "./pages/admin/layouts/components/dashboard/Dashboard";
+import Crm from "./pages/admin/layouts/components/crm/Crm";
+import Blog from "./pages/admin/layouts/components/blog/Blog";
+import Elearning from "./pages/admin/layouts/components/elearning/Elearning";
+import Ecommerce from "./pages/admin/layouts/components/ecommerce/Ecommerce";
+import Seo from "./pages/admin/layouts/components/seo/Seo";
+import CMS from "./pages/admin/layouts/components/cms/CmsPages"
+import Billing from "./pages/admin/layouts/components/billing/Billing";
 
 const  App = () => {
   return (
     <div className="App relative">
       <Router>
-        <div className="flex gap-9 bg-f4f5f6 h-100 overflow-hidden ">
+        <div className="flex gap-9 bg-e8f2fc h-100 overflow-hidden ">
           <div className="w-side">
             <Sidebar />
           </div>
@@ -18,6 +25,15 @@ const  App = () => {
               {/* ======================= Start-pages ======================= */}
               <Route path="/" element={<Dashboard />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/crm" element={<Crm />} />
+              <Route path="/blogs" element={<Blog />} />
+              <Route path="/elearning" element={<Elearning />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+              <Route path="/elearning" element={<Elearning />} />
+              <Route path="/cms" element={<CMS />} />
+              <Route path="/seo" element={<Seo />} />
+              <Route path="/cms" element={<CMS />} />
+              <Route path="/billing" element={<Billing />} />
               {/* ======================= End-pages ======================= */}
 
               {/* ======================= Start-Error ======================= */}
--- src/pages/admin/sidebar/Sidebar.js
+++ src/pages/admin/sidebar/Sidebar.js
@@ -3,6 +3,54 @@ import FeatherIcon from "feather-icons-react";
 import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const sidebarmenu = [
+    {
+      id: 1,
+      name: "Dashboard",
+      icon: "grid",
+      route: "dashboard",
+    },
+    {
+      id: 2,
+      name: "Blog View",
+      icon: "folder",
+      route: "crm",
+    },
+  ];
+  const sidebarmenu2 = [
+    {
+      id: 1,
+      name: "Category",
+      icon: "filter",
+      route: "blogs",
+    },
+    {
+      id: 2,
+      name: "Add Blogs",
+      icon: "plus",
+      route: "elearning",
+    },
+    {
+      id: 3,
+      name: "Edit Blogs",
+      icon: "edit",
+      route: "ecommerce",
+    },
+  ];
+  const sidebarmenu3 = [
+    {
+      id: 1,
+      name: "Blog Version",
+      icon: "user-plus",
+      route: "cms",
+    },
+    {
+      id: 2,
+      name: "Documentation",
+      icon: "file",
+      route: "seo",
+    },
+  ];
   return (
     <div className="bgwhite h-100 relative">
       <div className="">
@@ -13,81 +61,108 @@ const Sidebar = () => {
             className="sidebar-logo"
           />
         </div>
-        <div className="ptpx10 pbpx10 plpx20 prpx20 grid grid-cols-1 gap-12">
-          <p className="fsize15 font-600 textforth">Home</p>
-          <NavLink
-            activeClassName="active"
-            className="flex items-center textsidebar pbpx7 gap-9"
-            to="/"
-          >
-            <FeatherIcon icon="user" className=" cursor-pointer" size={16} />
-            <p className="fsize14 mtpx1 font-300">Dashboard</p>
-          </NavLink>
-        </div>
-        <div className="ptpx10 pbpx10 plpx20 prpx20 grid grid-cols-1 gap-12">
-          <p className="fsize15 font-600 textforth">Blog Management</p>
-          <NavLink
-            activeClassName="active"
-            className="flex items-center textsidebar pbpx7 gap-9"
-            to="/add-category"
-          >
-            <FeatherIcon
-              icon="user-plus"
-              className=" cursor-pointer"
-              size={16}
-            />
-            <p className="fsize14 mtpx1 font-300">Add Category</p>
-          </NavLink>
-          <NavLink
-            activeClassName="active"
-            className="flex items-center textsidebar pbpx7 gap-9"
-            to="/add-blog"
-          >
-            <FeatherIcon
-              icon="user-plus"
-              className=" cursor-pointer"
-              size={16}
-            />
-            <p className="fsize14 mtpx1 font-300">Add Blogs</p>
-          </NavLink>
-          <NavLink
-            activeClassName="active"
-            className="flex items-center textsidebar pbpx7 gap-9"
-            to="/edit-blog"
-          >
-            <FeatherIcon
-              icon="user-plus"
-              className=" cursor-pointer"
-              size={16}
-            />
-            <p className="fsize14 mtpx1 font-300">Edit Blogs</p>
-          </NavLink>
-          <NavLink
-            activeClassName="active"
-            className="flex items-center textsidebar pbpx7 gap-9"
-            to="/status"
-          >
-            <FeatherIcon
-              icon="user-plus"
-              className=" cursor-pointer"
-              size={16}
-            />
-            <p className="fsize14 mtpx1 font-300">Status</p>
-          </NavLink>
-        </div>
-        <div className="ptpx10 pbpx10 plpx20 prpx20 grid grid-cols-1 gap-12">
-          <p className="fsize15 font-600 textforth">Features</p>
-          <NavLink
-            activeClassName="active"
-            className="flex items-center textsidebar pbpx7 gap-9"
-            to="/variation"
-          >
-            <FeatherIcon icon="user" className=" cursor-pointer" size={16} />
-            <p className="fsize14 mtpx1 font-300">Variation</p>
-          </NavLink>
+        <div className="ptpx10 pbpx10 plpx10 prpx10">
+          <div class="accordion">
+            <div class="tab">
+              <input type="checkbox" name="accordion-1" id="cb1" />
+              <label
+                for="cb1"
+                className="p10 hover-col cursor-pointer flex justify-between items-center fsize15 textforth mtpx2 font-300"
+              >
+                <div className="flex items-center gap-4">
+                  <FeatherIcon
+                    icon="home"
+                    className="textgray cursor-pointer"
+                    size={16}
+                  />
+                  Home
+                </div>
+                <div className="flex items-center gap-4">
+                  <p className="fsize12 dot-dash rounded-full flex items-center justify-center textwhite bgprimary">
+                    {sidebarmenu.length}
+                  </p>
+                  <FeatherIcon size="16" icon="chevron-right" />
+                </div>
+              </label>
+              <div class="tab__content">
+                <div className="grid grid-cols-1 p9 gap-12">
+                  {sidebarmenu.map((e) => (
+                    <NavLink
+                      activeClassName="active"
+                      className="flex items-center activesidebar pbpx7 gap-9"
+                      to={e.route}
+                    >
+                      <FeatherIcon
+                        icon={e.icon}
+                        className=" cursor-pointer"
+                        size={16}
+                      />
+                      <p className="fsize14  mtpx2 font-300">{e.name}</p>
+                    </NavLink>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div class="tab">
+              <input type="checkbox" name="accordion-2" id="cb2" />
+              <label
+                for="cb2"
+                className="p10 hover-col cursor-pointer flex justify-between items-center fsize15 textforth mtpx2 font-300"
+              >
+                <div className="flex items-center gap-4">
+                  <FeatherIcon
+                    icon="briefcase"
+                    className="textgray cursor-pointer"
+                    size={16}
+                  />
+                  Management
+                </div>
+                <div className="flex items-center gap-4">
+                  <p className="fsize12 dot-dash rounded-full flex items-center justify-center textwhite bgprimary">
+                    {sidebarmenu2.length}
+                  </p>
+                  <FeatherIcon size="16" icon="chevron-right" />
+                </div>
+              </label>
+              <div class="tab__content">
+                <div className="grid grid-cols-1 p9 gap-12">
+                  {sidebarmenu2.map((e) => (
+                    <NavLink
+                      activeClassName="active"
+                      className="flex items-center activesidebar pbpx7 gap-9"
+                      to={e.route}
+                    >
+                      <FeatherIcon
+                        icon={e.icon}
+                        className=" cursor-pointer"
+                        size={16}
+                      />
+                      <p className="fsize14  mtpx2 font-300">{e.name}</p>
+                    </NavLink>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 p9 gap-12">
+            {sidebarmenu3.map((e) => (
+              <NavLink
+                activeClassName="active"
+                className="flex items-center activesidebar pbpx7 gap-9"
+                to={e.route}
+              >
+                <FeatherIcon
+                  icon={e.icon}
+                  className=" cursor-pointer"
+                  size={16}
+                />
+                <p className="fsize14  mtpx2 font-300">{e.name}</p>
+              </NavLink>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0">
+      <div className="absolute bottom-0 left-0 w-full">
         <div className="ptpx10 pbpx10 plpx20 prpx20 grid grid-cols-1 gap-12">
           <div className="flex items-center pbpx7 gap-9">
             <FeatherIcon
